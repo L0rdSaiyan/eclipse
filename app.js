e602330067b8412ed7d724a58a65b7473faed9ad
joao victor <thehoodgamer20@gmail.com>
2022-09-04 19:31:46 -0300

    adicionado javascript

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function eclipse(){
+
+    let body = document.body
+    let sol = document.createElement('div')
+    let lua = document.createElement('div')
+    body.appendChild(sol)
+    body.appendChild(lua)
+    sol.id = "sol"
+    lua.id = "lua"
+}
